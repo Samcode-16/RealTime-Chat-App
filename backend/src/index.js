2ee012ace155795b82155643eea95c9f3d0ff159
Samudyatha K Bhat <samudyathakb.bca24@chanakyauniversity.edu.in>
2025-11-05 18:38:53 +0530

    Added socket.io into the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 
 dotenv.config();
-const app = express();
+
+
 const PORT = process.env.PORT || 5001;
 
 // Connect to MongoDB
@@ -34,6 +39,6 @@ app.use(fileUpload({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
